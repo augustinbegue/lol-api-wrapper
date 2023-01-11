8cd56a39f318aa47148bbc2810ae7cec8a346f0e
Augustin BÉGUÉ <augustin.begue@epita.fr>
2023-01-11 08:32:05 +0100

    0.0.4: removed error log for 404

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,32 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 13,
-        'sourceType': 'module'
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
     },
-    'plugins': [
-        '@typescript-eslint'
+    "plugins": [
+        "@typescript-eslint"
     ],
-    'rules': {
-        'indent': [
-            'error',
+    "rules": {
+        "indent": [
+            "error",
             4
         ],
-        'quotes': [
-            'error',
-            'single'
+        "quotes": [
+            "error",
+            "double"
         ],
-        'semi': [
-            'error',
-            'always'
+        "semi": [
+            "error",
+            "always"
         ]
     }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
-import * as dotenv from 'dotenv';
-import type { CurrentGameInfo, LeagueEntryDTO, MatchDTO, MatchTimelineDTO, SummonerDTO } from '../types';
+import axios, {
+    AxiosError,
+    AxiosInstance,
+    AxiosRequestConfig,
+    AxiosResponse,
+} from "axios";
+import * as dotenv from "dotenv";
+import type {
+    CurrentGameInfo,
+    LeagueEntryDTO,
+    MatchDTO,
+    MatchTimelineDTO,
+    SummonerDTO,
+} from "../types";
 
 dotenv.config();
 
@@ -13,11 +24,15 @@ class DDragonWrapper {
     protected champions: any;
 
     protected static async getLatestVersion(): Promise<string> {
-        const response = await axios.get('https://ddragon.leagueoflegends.com/api/versions.json');
+        const response = await axios.get(
+            "https://ddragon.leagueoflegends.com/api/versions.json",
+        );
         return response.data[0];
     }
 
-    private async RIOTDataRequest(path: string): Promise<AxiosResponse | undefined> {
+    private async RIOTDataRequest(
+        path: string,
+    ): Promise<AxiosResponse | undefined> {
         try {
             const response = await this.datareq.get(path);
             return response;
@@ -29,13 +44,15 @@ class DDragonWrapper {
     }
 
     protected async cacheChampions() {
-        const response = await this.RIOTDataRequest('champion.json');
+        const response = await this.RIOTDataRequest("champion.json");
         return response?.data.data;
     }
 
     getChampionById(id: number) {
         if (!this.champions) {
-            console.error('cache not populated, please build this class first.');
+            console.error(
+                "cache not populated, please build this class first.",
+            );
             return;
         }
 
@@ -58,65 +75,91 @@ class DDragonWrapper {
 }
 
 export class RiotWrapper extends DDragonWrapper {
-    private async ApiRequest(path: string, instance: AxiosInstance): Promise<AxiosResponse | undefined> {
+    private async ApiRequest(
+        path: string,
+        instance: AxiosInstance,
+    ): Promise<AxiosResponse | undefined> {
         try {
             const response = await instance.get(path);
             return response;
         } catch (error) {
             const err = error as AxiosError;
-            console.error(`Error on ${path} with status`, err.response?.status, ':');
-            if (err.response) {
-                console.error(err.response.data.status.message);
-            } else {
-                console.error(err.message);
+
+            if (err.response?.status && err.response.status != 404) {
+                console.error(
+                    `Error on ${path} with status`,
+                    err.response.status,
+                    ":",
+                );
+                if (err.response) {
+                    console.error(err.response.data.status.message);
+                } else {
+                    console.error(err.message);
+                }
             }
             return undefined;
         }
     }
 
-    private apiKey = '';
-    private euw1endpoint = 'https://euw1.api.riotgames.com';
+    private apiKey = "";
+    private euw1endpoint = "https://euw1.api.riotgames.com";
     private euw1config: AxiosRequestConfig;
     euw1req: AxiosInstance;
-    private europeendpoint = 'https://europe.api.riotgames.com';
+    private europeendpoint = "https://europe.api.riotgames.com";
     private europeconfig: AxiosRequestConfig;
     europereq: AxiosInstance;
 
-    private async EUW1ApiRequest(path: string): Promise<AxiosResponse | undefined> {
+    private async EUW1ApiRequest(
+        path: string,
+    ): Promise<AxiosResponse | undefined> {
         return this.ApiRequest(path, this.euw1req);
     }
 
-    private async EUROPEApiRequest(path: string): Promise<AxiosResponse | undefined> {
+    private async EUROPEApiRequest(
+        path: string,
+    ): Promise<AxiosResponse | undefined> {
         return this.ApiRequest(path, this.europereq);
     }
 
     async getSummonerIdsByName(name: string): Promise<SummonerDTO | undefined> {
         name = encodeURI(name);
-        const response = await this.EUW1ApiRequest(`/lol/summoner/v4/summoners/by-name/${name}`);
+        const response = await this.EUW1ApiRequest(
+            `/lol/summoner/v4/summoners/by-name/${name}`,
+        );
 
         return response?.data;
     }
 
-    async getLeagueEntryBySummonerId(summonerId: string): Promise<LeagueEntryDTO[] | undefined> {
-        const response = await this.EUW1ApiRequest(`/lol/league/v4/entries/by-summoner/${summonerId}`);
+    async getLeagueEntryBySummonerId(
+        summonerId: string,
+    ): Promise<LeagueEntryDTO[] | undefined> {
+        const response = await this.EUW1ApiRequest(
+            `/lol/league/v4/entries/by-summoner/${summonerId}`,
+        );
 
         return response?.data;
     }
 
     async getActiveGame(summonerId: string): Promise<CurrentGameInfo> {
-        const response = await this.EUW1ApiRequest(`/lol/spectator/v4/active-games/by-summoner/${summonerId}`);
+        const response = await this.EUW1ApiRequest(
+            `/lol/spectator/v4/active-games/by-summoner/${summonerId}`,
+        );
 
         return response?.data;
     }
 
     async getMatchById(matchId: string): Promise<MatchDTO> {
-        const response = await this.EUROPEApiRequest(`/lol/match/v5/matches/${matchId}`);
+        const response = await this.EUROPEApiRequest(
+            `/lol/match/v5/matches/${matchId}`,
+        );
 
         return response?.data;
     }
 
     async getMatchTimelineById(matchId: string): Promise<MatchTimelineDTO> {
-        const response = await this.EUROPEApiRequest(`/lol/match/v5/matches/${matchId}/timeline`);
+        const response = await this.EUROPEApiRequest(
+            `/lol/match/v5/matches/${matchId}/timeline`,
+        );
 
         return response?.data;
     }
@@ -125,22 +168,22 @@ export class RiotWrapper extends DDragonWrapper {
         super(version);
 
         if (process.env.RIOT_API_KEY === undefined) {
-            console.error('No RIOT_API_KEY found in .env file.');
-            throw new Error('No RIOT_API_KEY found in .env file.');
+            console.error("No RIOT_API_KEY found in .env file.");
+            throw new Error("No RIOT_API_KEY found in .env file.");
         } else {
             this.apiKey = process.env.RIOT_API_KEY;
 
             this.europeconfig = {
                 baseURL: this.europeendpoint,
                 headers: {
-                    'X-Riot-Token': this.apiKey,
+                    "X-Riot-Token": this.apiKey,
                 },
             };
 
             this.euw1config = {
                 baseURL: this.euw1endpoint,
                 headers: {
-                    'X-Riot-Token': this.apiKey,
+                    "X-Riot-Token": this.apiKey,
                 },
             };
 
